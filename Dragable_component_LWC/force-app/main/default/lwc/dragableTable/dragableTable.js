7f5cedeb977b5dcc755e36e964c6571f0e1416d4
manish nagar <33551293+manishnagar1995@users.noreply.github.com>
2020-03-17 15:48:57 +0530

    final commit

--- Dragable_component_LWC/force-app/main/default/lwc/dragableTable/dragableTable.js
+++ Dragable_component_LWC/force-app/main/default/lwc/dragableTable/dragableTable.js
@@ -2,26 +2,28 @@
 import { LightningElement, wire, track} from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
 import { fireEvent } from 'c/pubsub';
+import { registerListener, unregisterAllListeners } from 'c/pubsub';
 import getcase from '@salesforce/apex/AccountHelper.getAccountLocations';
 
 let i=0;
 let caseIds; //store id from the div
 let indexs;
+let datare;
 let selectedcase; //store selected account
 
 let datalist =[
     {
-        Id: 1,
+        Id: 5,
         Name: 'Amy Taylor',
         Title: 'VP of Engineering',
     },
     {
-        Id: 2,
+        Id: 6,
         Name: 'Michael Jones',
         Title: 'VP of Sales',
     },
     {
-        Id: 3,
+        Id: 7,
         Name: 'Jennifer Wu',
         Title: 'CEO',
     },
@@ -31,38 +33,57 @@ export default class DragableTable extends LightningElement {
     @track record =  datalist;
      //get page reference
      @wire(CurrentPageReference) pageRef;
+     returnInfo;
      constructor() {
          super();
          //register dragover event to the template
          this.template.addEventListener('dragover', this.handleDragOver.bind(this));
      }
    
+     connectedCallback() {
+        // subscribe to selectedAccountDrop event
+        registerListener('returnDataSend', this.handlereturnDataSend, this);
+      }
+   
+    disconnectedCallback() {
+        // unsubscribe from selectedAccountDrop event
+        unregisterAllListeners(this);
+    }
+    handlereturnDataSend(returnInfo){
+        this.returnInfo = returnInfo;
+        console.log('=====test here ==============',this.returnInfo);
+        for(i=0; i< this.record.length; i++) {
+            if( this.returnInfo == this.record[i].Id  &&  this.returnInfo != null ){
+                console.log('=====test here2 ==============',indexs); 
+                this.record.splice(indexs, 1);   
+            }    
+          }
+      }
+
     
 
     handleDragStart(event) {
         event.dataTransfer.dropEffect = 'move';
-      
         caseIds = event.target.dataset.item;
         indexs = event.target.dataset.index;
-        console.log('event.target.dataset.item=' + event.target.dataset.item);
+        console.log('event.target.dataset.item=' ,event.target.dataset.item);
+        console.log('event.target.dataset.index=' ,event.target.dataset.index);
  
         console.log('length=:' + this.record.length);
         for(i=0; i< this.record.length; i++) {
            if(caseIds == this.record[i].Id  && caseIds != null ){
             
             selectedcase =this.record[i];  
-          //  record.splice(indexs, i);
-            console.log('test==============>',JSON.stringify(selectedcase));
+           console.log('test==============>',JSON.stringify(selectedcase));
+           fireEvent(this.pageRef, 'selectedCaseDrop', selectedcase); 
            }    
-                                                              
-        } 
-     
-       fireEvent(this.pageRef, 'selectedCaseDrop', selectedcase);   
+         }
      }
 
     handleDragOver(event){
         event.dataTransfer.dropEffect = 'move';
-        event.preventDefault();       
+        event.preventDefault();    
+     //   this.record.splice(indexs, 1);     
     } 
 
 }
--- Dragable_component_LWC/force-app/main/default/lwc/dropTable/dropTable.js
+++ Dragable_component_LWC/force-app/main/default/lwc/dropTable/dropTable.js
@@ -1,9 +1,18 @@
 /* eslint-disable no-console */
 import { LightningElement, wire, track } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
+import { fireEvent } from 'c/pubsub';
 import { registerListener, unregisterAllListeners } from 'c/pubsub';
 
-let data;
+let data = '';
+let returnData;
+
+let i=0;
+let caseIds; //store id from the div
+let indexs;
+let index1;
+let index2;
+let temp;
 
 const parentList = [ {
                         "firstList" : [{
@@ -86,19 +95,58 @@ export default class DropTable extends LightningElement {
     handleSelectedCaseDrop(caseInfo){
       this.caseInfo = caseInfo;
     }
+    allowDrop(event){
+        event.preventDefault();
+    }
 
+    handleDragStart(event) {
+        event.dataTransfer.dropEffect = 'move';
+        caseIds = event.target.dataset.item;
+        indexs = event.target.dataset.index;
+        event.dataTransfer.setData("text", caseIds)
+        
+     }
     handleDrop(event){
-        console.log('inside handle drop'); 
         if(event.stopPropagation){
             event.stopPropagation();
         }
-        event.preventDefault();
+        
+        var data1 = event.dataTransfer.getData("text");
+        console.log('dsdsf',data1);
+        var tar = event.currentTarget.dataset.item;
+        console.log('cloest id',tar);
+        for(i=0; i< this.DroppedcaseNumber.length; i++) {
+            if(this.DroppedcaseNumber[i].Id==data1) index1 = i; 
+            if(this.DroppedcaseNumber[i].Id==tar) index2 = i;
+        }
+        
+        console.log('index11',index1);
+        console.log('index22',index2);
+        if(index1<index2) {
+            // Lower index to higher index; we move the lower index first, then remove it.
+            this.DroppedcaseNumber.splice(index2+1, 0, this.DroppedcaseNumber[index1]);
+            this.DroppedcaseNumber.splice(index1, 1);
+        } else {
+            // Higher index to lower index; we remove the higher index, then add it to the lower index.
+            temp = this.DroppedcaseNumber.splice(index1, 1)[0];
+            this.DroppedcaseNumber.splice(index2, 0, temp);
+        }
+    
         data = this.caseInfo.Name;
-        alert(data);
-        console.log('inside handle  data', data); 
+        console.log('inside handle  data'); 
+        console.log('tweset if data',data != null );
+        console.log('tweset if',data1 != null);
         if( data != null){
-            this.DroppedcaseNumber.push(this.caseInfo);
+            returnData = this.caseInfo.Id;
+            console.log('returndata',returnData);
+            this.DroppedcaseNumber.splice(tar, 0, this.caseInfo);
+            data =null;
+            this.caseInfo = null;
+        //   this.DroppedcaseNumber.splice(data1, 1);
+            fireEvent(this.pageRef, 'returnDataSend', returnData);  
          }
+        event.preventDefault();
+
       }
 
     handleDragOver(event){
